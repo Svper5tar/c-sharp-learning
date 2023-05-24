16c78ed6d254202bb8719b773def8b5cd084e5c2
Alexey <naverno1995@mail.ru>
2023-05-24 16:02:26 +0300

    add hometasks

---
+++ Hometasks/Lesson_5/task_2/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+
+int[] CreateArray(int min, int max, int size)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.WriteLine($"{array[i]}]");
+    }
+}
+
+
+int SumOfOddElements(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 != 0) sum = sum + array[i];
+    }
+    return sum;
+}
+
+
+int[] array = CreateArray(-10, 10, 10);
+PrintArray(array);
+Console.WriteLine(SumOfOddElements(array));
---
+++ Hometasks/Lesson_5/task_3/Program.cs
@@ -0,0 +1,62 @@
+﻿// Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+// [3.5, 7.1, 22.9, 2.3, 78.5] -> 76.2
+
+
+double[] CreateArray(double min, double max, int size)
+{
+    double[] array = new double[size];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Math.Round(rnd.NextDouble() * (max - min) + min, 1);
+    }
+    return array;
+}
+
+
+void PrintArray(double[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.WriteLine($"{array[i]}]");
+    }
+}
+
+
+double MaxElement(double[] array)
+{
+    double max = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > max) max = array[i];
+    }
+    return max;
+}
+
+
+double MinElement(double[] array)
+{
+    double min = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] < min) min = array[i];
+    }
+    return min;
+}
+
+
+double Difference(double min, double max)
+{
+    double difference = Math.Round(max - min, 1);
+    return difference;
+}
+
+
+double[] array = CreateArray(0, 100, 10);
+PrintArray(array);
+Console.WriteLine($"Минимальный элемент массива: {MinElement(array)}");
+Console.WriteLine($"Максимальный элемент массива: {MaxElement(array)}");
+Console.WriteLine($"Разница между максимальным и минимальным элементами массива: {Difference(MinElement(array), MaxElement(array))}");
