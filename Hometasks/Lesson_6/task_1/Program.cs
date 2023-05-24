21891669cf80c4e065ade139f9f551b7d05a6db4
Alexey <naverno1995@mail.ru>
2023-05-24 19:54:13 +0300

    add hometasks

---
+++ Hometasks/Lesson_6/task_1/Program.cs
@@ -0,0 +1,46 @@
+﻿// Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Сколько элементов будет в массиве?");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = CreateArray(size);
+PrintArray(array);
+Console.WriteLine($"Чисел в массиве больше 0: {NumberGreaterZero(array)}");
+
+int[] CreateArray(int size)
+{
+    int[] array = new int[size];
+    int i = 0;
+    while (i < size)
+    {
+        Console.Write($"Введите число номер {i+1}: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine();
+        i++;
+    }
+    return array;
+}
+
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < (array.Length); i++)
+    {
+        if (i < (array.Length - 1)) Console.Write($"{array[i]}, ");
+        else Console.WriteLine($"{array[i]}]");    
+    }
+}
+
+
+int NumberGreaterZero(int[] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) result++;
+    }
+    return result;
+}
